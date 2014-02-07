9787682dd4d5237be3985e9e67393d8c86417cc7
Vitaliy Novoselskiy <vit4lik93@gmail.com>
2014-02-07 18:59:46 +0200

    Check HttpClient

--- src/bookmaker/Application.java
+++ src/bookmaker/Application.java
@@ -6,6 +6,11 @@
 
 package bookmaker;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author vitaliy
@@ -16,7 +21,25 @@ public class Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        org.apache.http.impl.client.CloseableHttpClient httpclient
+            = org.apache.http.impl.client.HttpClients.createDefault();
+        org.apache.http.client.methods.HttpGet httpget
+            = new org.apache.http.client.methods.HttpGet();
+        try {
+            httpget.setURI(
+                    new java.net.URI("http://myscore.ru")
+            );
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        org.apache.http.client.methods.CloseableHttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+        } catch (IOException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+            response = new org.apache.http.client.methods;
+        }
+        System.out.println(response.toString());
     }
     
 }
